68bc4daa21e5b5b840852e117a2b2b41f1f5578b
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2023-01-26 14:54:33 +0800

    Code for Merge Sort of the given array.

---
+++ merge.py
@@ -0,0 +1,57 @@
+def mergeSort(array):
+    if len(array) > 1:
+
+        #  r is the point where the array is divided into two subarrays
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
+
+        # Sort the two halves
+        mergeSort(L)
+        print(L)
+        mergeSort(M)
+        print(M)
+
+        i = j = k = 0
+
+        # Until we reach either end of either L or M, pick larger among
+        # elements L and M and place them in the correct position at A[p..r]
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+    
+
+        # When we run out of elements in either L or M,
+        # pick up the remaining elements and put in A[p..r]
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+            print(array)
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+            print(array)
+
+
+# Print the array
+def printList(array):
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print()
+
+
+# Driver program
+if __name__ == '__main__':
+    array = [62, 28, 41, 15, 12, 89, 78, 51, 35, 83]
+    print('Given Array:', array)
+    mergeSort(array)
+
+    print('Sorted array is:', array)
